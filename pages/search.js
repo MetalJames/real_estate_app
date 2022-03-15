3374b867ccb5670144bdc395043870e447a037cc
Volodymyr <volodyaruzhak@gmail.com>
2022-03-15 14:40:34 +0200

    half way done with searchfilters componnent, also add noresut image, also add more code to search.js

--- pages/search.js
+++ pages/search.js
@@ -4,6 +4,11 @@ import Image from 'next/image';
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
 
+import SearchFilters from '../components/SearchFilters';
+import Property from '../components/Property';
+
+import noresult from '../assets/images/noresult.svg'
+
 const Search = () => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
@@ -25,6 +30,17 @@ const Search = () => {
                 <Icon paddingLeft='2' w='7' as={BsFilter} />
             </Flex>
             {searchFilters && <SearchFilters />}
+            <Text fontSize='2xl' p='4' fontWeight='bold'>
+                Properties {router.query.purpose}
+            </Text>
+            <Flex flexWrap='wrap'>
+                {[].map((property) => <Property property={property} key={property.id} />)}
+            </Flex>
+            {[].length === 0 && (
+                <Flex justifyContent='center' alignItems='center' flexDirection='column' marginTop='5' marginBottom='5'>
+                    <Image src={noresult} alt='no result'/>
+                </Flex>
+            )}
         </Box>
     )
 }
